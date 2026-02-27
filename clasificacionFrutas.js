d72d9760ad858beabfd3be489d2b8595620ae7bc
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 14:34:46 -0600

    se agregan frutas

---
+++ clasificacionFrutas.js
@@ -0,0 +1,7 @@
+
+const frutas = ["manzana", "plátano", "naranja", "guayaba", "pera", "naranja", "guanabana","sandia", "melón", "papaya", "mandarina", "fresa", "manzana", "plátano", "naranja", "guayaba", "pera", "naranja", "guanabana","sandia", "melón", "papaya", "mandarina", "fresa", "manzana", "plátano", "naranja", "guanabana", "pera", "ciruela"];
+
+
+const contadorFrutas = {};
+
+
