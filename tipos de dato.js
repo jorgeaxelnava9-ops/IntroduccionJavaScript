d50010e97aead42b3221aeb0e5ff7cda7fd154d0
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 11:57:41 -0600

    se agregan más ejemplos

--- tipos de dato.js
+++ tipos de dato.js
@@ -14,3 +14,35 @@ console.log(typeof null);
 console.log(typeof undefined);
 console.log(typeof FALSE); 
 
+//Ejemplos
+
+console.log(555555555555 es tipo:", typeof 555555555555");
+
+console.log("-64.58 es tipo:", typeof-64.58);
+
+console.log("10 / 2 es tipo:", typeof (10 / 2));
+
+console.log("5 > 3 es tipo:", typeof (5 > 3));
+
+console.log("'Ejemplos' + 'JavaScript' es tipo:", typeof ("Ejemplos" + "Javascript"));
+
+let variableSin Valor;
+console.log("variableSinValor es tipo:", typeof variableSinValor);
+
+console.log("[1,2,3] es tipo:", typeof [1,2,3]);
+
+console.log("{nombre: 'Jorge'} es tipo:", typeof {nombre: "Jorge"});
+
+console.log("() => {} es tipo:", typeof (() => {}));
+
+console.log("Symbol('id') es tipo;", typeof Symbol("id"));
+
+console.log("123n es tipo:", typeof 123n);
+
+
+
+
+
+
+
+
