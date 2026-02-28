79152c0cfd917c8f75afd234c4543d4fb5e00a2b
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 22:25:51 -0600

    se agregan elementos DOM

--- IntroaDom/script.js
+++ IntroaDom/script.js
@@ -1,1 +1,6 @@
+//Se agregan elementos DOM
+const form = document.getElementById("commentForm");
+const input = document.getElementById("commentInput");
+const container = document.getElementById("commentsContainer");
 
+//Evento cuando se envía formulario
