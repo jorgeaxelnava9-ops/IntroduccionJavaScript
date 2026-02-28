129418484f100485f12b5573ddd65efe88c36d98
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 22:30:36 -0600

    se agrega instrucciones al formulario

--- IntroaDom/script.js
+++ IntroaDom/script.js
@@ -4,3 +4,16 @@ const input = document.getElementById("commentInput");
 const container = document.getElementById("commentsContainer");
 
 //Evento cuando se envía formulario
+form.addEventListener("submit"), function(event) {
+    event.preventDefault(); 
+
+
+const commentText = input.ariaValueMax.trim();
+
+if (commentText === "") {
+    alert("Por favor escribe un comentario");
+    return;
+}
+
+//Elementos del comentario
+}
