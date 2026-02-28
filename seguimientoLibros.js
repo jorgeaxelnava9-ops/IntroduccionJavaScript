899b02423c60f0192bfc9cc189f9ba8dfe6e3281
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 20:41:51 -0600

    se agregan libros

--- seguimientoLibros.js
+++ seguimientoLibros.js
@@ -8,7 +8,23 @@ function mostrarLibrosLeidos() {
     console.log("Libros leídos:");
 
     for (let i = 0; i < librosLeidos.length; i++) {
-        console.log("-" + librosLeidos[i]);
+        console.log("- " + librosLeidos[i]);
     }
-    
 }
+ 
+   //Aquí se agregan libros 
+
+    agregarLibro("Harry Potter");
+    agregarLibro("La Conspiración");
+    agregarLibro("Doctor Sueño");
+    agregarLibro("El Psicoanalista");
+    agregarLibro("It");
+    agregarLibro("El Visitante");
+    agregarLibro("Hábitos Atómicos");
+    agregarLibro("Don Quijote de la Mancha de Cervantes");
+    agregarLibro("El Señor de los Anillos");
+    agregarLibro("Pedro Páramo");
+    agregarLibro("El Jardín de las Mariposas");
+
+    //Aquí se muestran los libros fuera de la función
+mostrarLibrosLeidos();
