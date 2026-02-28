13d8a19a9f2ce354425f4213d1060f8e05cf2721
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-27 20:23:05 -0600

    se agrega función

--- seguimientoLibros.js
+++ seguimientoLibros.js
@@ -3,3 +3,12 @@ let librosLeidos = [];
 function agregarLibro(titulo) {
     librosLeidos.push(titulo);
 }
+
+function mostrarLibrosLeidos() {
+    console.log("Libros leídos:");
+
+    for (let i = 0; i < librosLeidos.length; i++) {
+        console.log("-" + librosLeidos[i]);
+    }
+    
+}
