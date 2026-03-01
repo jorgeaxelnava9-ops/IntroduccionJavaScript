68db9ca8f7cad7df026d8056a5d2fc237d769a73
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-02-28 21:02:04 -0600

    se agrega condiciones en JS

---
+++ Generador-Contrasena/script.js
@@ -0,0 +1,7 @@
+const passwordInput = document.getElementById("password");
+const lengthInput = document.getElementById("length");
+const lengthValue = document.getElementById("lengthValue");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
